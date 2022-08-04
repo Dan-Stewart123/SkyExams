51f4d1b0f7dfcea50423604660f132f6d552670b
Dan-Stewart123 <u20428660@tuks.co.za>
2022-08-04 16:26:07 +0200

    Student report Done

---
+++ SkyExams/ViewModels/StudentVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SkyExams.Models
+
+namespace SkyExams.ViewModels
+{
+    public class StudentVM
+    {
+        public string title { get; set; }
+        public string name { get; set; }
+        public string licence { get; set; }
+        public string hoursFlown { get; set; }
+        public ICollection<Plane_Type> planeTypes { get; set; }
+        public ICollection<Student_Exam> examMarks { get; set; }
+    }
+}
